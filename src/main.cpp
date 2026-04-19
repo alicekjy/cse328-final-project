fceb4448bd3f281e7dd7760afe12d55cfe7c9149
Alice Kim <jaeyoung.kim.1@stonybrook.edu>
2026-04-19 02:16:35 -0400

    Finalizing phase 1

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Mac needs this
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); 
 
     // Create window
     GLFWwindow* window = glfwCreateWindow(800, 600, "X-Toon Shader", NULL, NULL);
